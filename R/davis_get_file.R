992f4f1557a739f5c156eea70ba02fd687abcbe4
Karthik Chandrasekar <karthidec1@gmail.com>
2016-03-16 12:05:30 -0700

    Update davis_get_file.R

--- R/davis_get_file.R
+++ R/davis_get_file.R
@@ -1,6 +1,6 @@
 library("httr")
 source("R/config.R")
-davis_get <- function(file_name){
+davis_get_file <- function(file_name){
     get_end_point = paste(server_address,sprintf(get_end_point, file_name), sep="")
     GET(get_end_point, config=add_headers(Authorization=token_header))
 }
